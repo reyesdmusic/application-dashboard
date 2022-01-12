3f91c25d7146f87ebce247fee2bbda6a4622dfc9
Danny Reyes <reyesdmusic@gmail.com>
2022-01-12 11:00:20 -0500

    Create more data records

--- application-dashboard/src/mock-applications.ts
+++ application-dashboard/src/mock-applications.ts
@@ -89,4 +89,254 @@ export const APPLICATIONS: Application[] = [
          }
       ]
    }
+
+
+
+
+
+   /////////////
+
+   ,
+   {
+      "id":5,
+      "name":"Margarita Gonzalez",
+      "position":"Host",
+      "applied":"05/08/16",
+      "experience":2,
+      "availability":{
+         "M":1,
+         "T":0,
+         "W":1,
+         "Th":0,
+         "F":1,
+         "S":1,
+         "Su":1
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"Yes"
+         }
+      ]
+   },
+   {
+      "id":6,
+      "name":"Tom Wambsgans",
+      "position":"Line Cook",
+      "applied":"04/15/16",
+      "experience":1,
+      "availability":{
+         "M":1,
+         "T":1,
+         "W":1,
+         "Th":0,
+         "F":0,
+         "S":0,
+         "Su":0
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"No"
+         }
+      ]
+   },
+   {
+      "id":7,
+      "name":"Shiv Roy",
+      "position":"Cook",
+      "applied":"05/01/16",
+      "experience":3,
+      "availability":{
+         "M":0,
+         "T":1,
+         "W":1,
+         "Th":0,
+         "F":1,
+         "S":1,
+         "Su":0
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"Yes"
+         }
+      ]
+   },
+   {
+      "id":8,
+      "name":"Kendall Roy",
+      "position":"Chef",
+      "applied":"03/10/16",
+      "experience":5,
+      "availability":{
+         "M":1,
+         "T":1,
+         "W":1,
+         "Th":1,
+         "F":1,
+         "S":0,
+         "Su":0
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"No"
+         }
+      ]
+   },
+   {
+      "id":9,
+      "name":"Gerri Smith",
+      "position":"Shift Manager",
+      "applied":"05/22/16",
+      "experience":3,
+      "availability":{
+         "M":1,
+         "T":0,
+         "W":1,
+         "Th":0,
+         "F":1,
+         "S":1,
+         "Su":1
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"Yes"
+         }
+      ]
+   },
+   {
+      "id":10,
+      "name":"Greg Roy",
+      "position":"Host",
+      "applied":"05/06/16",
+      "experience":5,
+      "availability":{
+         "M":0,
+         "T":0,
+         "W":1,
+         "Th":1,
+         "F":1,
+         "S":0,
+         "Su":0
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"No"
+         }
+      ]
+   },
+   {
+      "id":11,
+      "name":"Rhea Jarrell",
+      "position":"Server",
+      "applied":"03/08/16",
+      "experience":1,
+      "availability":{
+         "M":1,
+         "T":0,
+         "W":1,
+         "Th":0,
+         "F":1,
+         "S":0,
+         "Su":0
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"Yes"
+         }
+      ]
+   },
+   {
+      "id":12,
+      "name":"Logan Roy",
+      "position":"Cook",
+      "applied":"07/08/16",
+      "experience":12,
+      "availability":{
+         "M":1,
+         "T":1,
+         "W":1,
+         "Th":1,
+         "F":1,
+         "S":1,
+         "Su":1
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"Yes"
+         }
+      ]
+   },
+   {
+      "id":13,
+      "name":"Kevin Smith",
+      "position":"Busser",
+      "applied":"02/18/16",
+      "experience":1,
+      "availability":{
+         "M":0,
+         "T":0,
+         "W":0,
+         "Th":0,
+         "F":1,
+         "S":1,
+         "Su":1
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"Yes"
+         }
+      ]
+   },
+   {
+      "id":14,
+      "name":"Janet Carlotti",
+      "position":"Busser",
+      "applied":"04/28/16",
+      "experience":2,
+      "availability":{
+         "M":1,
+         "T":0,
+         "W":0,
+         "Th":1,
+         "F":1,
+         "S":0,
+         "Su":0
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"Yes"
+         }
+      ]
+   },
+   {
+      "id":15,
+      "name":"Jennifer O'Keefe",
+      "position":"Shift Manager",
+      "applied":"05/12/16",
+      "experience":4,
+      "availability":{
+         "M":1,
+         "T":0,
+         "W":1,
+         "Th":0,
+         "F":1,
+         "S":1,
+         "Su":0
+      },
+      "questions":[
+         {
+            "text":"Are you authorized to work in the United States?",
+            "answer":"No"
+         }
+      ]
+   }
 ]
